714788c76695f28c8b77553839ba641750d3500d
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-07-29 14:12:58 +0100

    a new day push

---
+++ js/musiclist.js
@@ -0,0 +1,74 @@
+let allMusic = [
+  {
+    name: "Fragrance-To-Fire - Gospel",
+    artist: "Dunsin Oyekan",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Amazing-God - Gospel",
+    artist: "Dunsin Oyekan",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Ga-Shi-Nan - Home",
+    artist: "Kaestrings",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Firm-Foundation - Home",
+    artist: "Maverick-City",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Eze - Home",
+    artist: "Mercy-Chinwo",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Oceans - Home",
+    artist: "Hilsong",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "Bola - Home",
+    artist: "Sunmisola-Agbebi",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name: "Im-Getting-Ready - Home",
+    artist: "Tasha-Cobbs ft Nicki",
+    img: "music-8",
+    src: "music-8",
+  },
+  {
+    name: "Stay-Here - Home",
+    artist: "Travis-Greene",
+    img: "music-9",
+    src: "music-9",
+  },
+  {
+    name: "Amen-Amen - Home",
+    artist: "TY-Bello",
+    img: "music-10",
+    src: "music-10",
+  },
+  {
+    name: "I-Get-Backing - Home",
+    artist: "Victoria-Orenze",
+    img: "music-11",
+    src: "music-11",
+  },
+  {
+    name: "What-A-Beautiful-Name - Home",
+    artist: "Hillsong",
+    img: "music-12",
+    src: "music-12",
+  },
+];
